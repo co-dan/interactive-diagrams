21cb294179a244af4429c6b461d2351503223c2d
Daniil Frumin <difrumin@gmail.com>
2013-08-10 18:36:53 +0400

    Adding 'getDataSafe' to 'System.Restricted.Worker.Protocol'

--- restricted-workers/src/System/Restricted/Worker/Protocol.hs
+++ restricted-workers/src/System/Restricted/Worker/Protocol.hs
@@ -1,15 +1,24 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 -- | A simple protocol for sending serializable data over handles
+--
+-- Please note that this is a very simple implementation that works
+-- fine for most of that data, however, the size of the data you might
+-- send at one go is limited to MAX_WORD32 bytes. We use 'cereal' for
+-- serialization.
 module System.Restricted.Worker.Protocol
     (
       sendData
     , getData
+    , getDataSafe
     , DecodeResult
+    , ProtocolException(..)
     ) where
 
 import           Control.Applicative            ((<$>))
 import           Control.Exception              (IOException, catch, throw)
+import           Control.Monad.Trans            (lift)
+import           Control.Monad.Trans.Either     (hoistEither, left, runEitherT)
 import           Data.ByteString                (ByteString, hGet, hPut)
 import qualified Data.ByteString                as BS
 import           Data.Serialize                 (Serialize, decode, encode)
@@ -22,17 +31,20 @@ import           System.Restricted.Worker.Types
 type DecodeResult a = Either String a
 
 -- | Send some serialiazable data over a handle.
--- Returns 'ByteString' representing the encoded data
+-- Returns 'ByteString' representing the encoded data. May throw
+-- 'ProtocolException'
 sendData :: Serialize a => Handle -> a -> IO ByteString
 sendData h d = sendData' h d
                `catch` \(e :: IOException) ->
                throw (HandleException e)
 
 -- | Read the data from a handle and deserialize it.
+-- May throw 'ProtocolException'
 getData :: Serialize a => Handle -> IO a
 getData h = getData' h
             `catch` \(e :: IOException) ->
             throw (HandleException e)
+
 
 sendData' :: Serialize a => Handle -> a -> IO ByteString
 sendData' hndl datum = do
@@ -54,3 +66,13 @@ getData' hndl = do
     case res of
         Left str -> throw (ConversionException $ "Deserialization error:\n" ++ str)
         Right x  -> return x
+
+-- | Safe version of 'getData' that doesn't throw 'ProtocolException'
+getDataSafe :: Serialize a => Handle -> IO (DecodeResult a)
+getDataSafe hndl = runEitherT $ do
+    lenD :: DecodeResult Word32 <- decode <$> lift (hGet hndl 4)
+    case lenD of
+        Left str -> left $ "Conversion error while reading length: " ++ str
+        Right len ->
+            hoistEither =<< decode <$> (lift (hGet hndl (fromIntegral len)))
+
