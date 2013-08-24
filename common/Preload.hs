5063fa23ba311ed5e23d5494cb826ccb55b411d5
Dan Frumin <difrumin@gmail.com>
2013-08-24 10:08:42 +0000

    Fixing .gitignore, Preload.hs & eval-api.cabal

--- common/Preload.hs
+++ common/Preload.hs
@@ -14,18 +14,8 @@ allExceptions = return . display
 
 preload :: (Diagram SVG R2, Int)
 preload = 
-  let d = dia 50
+  let d = circle 1
       x = 100 + 100
   in (d, x)
 
   
-dia :: Int -> Diagram SVG R2
-dia n = polygon
-  with { polyType =
-            PolyPolar (repeat (frac n))
-            (take n (cycle [1,3,1,4,2,1]))
-       } 
-
-frac :: Int -> CircleFrac
-frac n = CircleFrac $ fromRational
-         (1/(toRational n))
